269e601ed8755fde913adb024d1e060b3b95f626
Ash Faisal <ashfaisal.dev@gmail.com>
2025-03-21 01:23:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+
+const { connectDB } = require('./src/config/database');
+const authRouter = require('./src/routers/auth');
+const cookieParser = require('cookie-parser');
+const userRouter = require('./src/routers/user');
+const songRouter = require('./src/routers/song');
+const errorHandler = require('./src/middlewares/errorHandler');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+app.use('/auth', authRouter);
+app.use('/user', userRouter);
+app.use('/song', songRouter);
+
+// handling errors
+app.use(errorHandler);
+
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log('listening for requests');
+  });
+});
